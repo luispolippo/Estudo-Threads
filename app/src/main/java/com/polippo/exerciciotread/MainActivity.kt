9522ff2273219006a8ac0f0d977b85ce566daa68
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-23 17:31:25 -0300

    created functions to async tasks

--- app/src/main/java/com/polippo/exerciciotread/MainActivity.kt
+++ app/src/main/java/com/polippo/exerciciotread/MainActivity.kt
@@ -1,25 +1,68 @@
 package com.polippo.exerciciotread
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO: 18 - fazer o handle do clique do botão
+
+        button_load_data.setOnClickListener {
+            launchAstrosTask()
+        }
     }
 
 
-    //TODO: 13 - criar função para exibir os dados carregados
+    fun showData(list: List<AstrosPeople>){
+        textView_data.text = ""
+        list?.forEach{ people ->
+            textView_data.append("${people.name} - ${people.craft} \n\n")
+        }
+    }
 
-    //TODO: 14 - criar função para exibir progressBar
 
-    //TODO: 15 - criar função para esconder progressBar
+    fun showLoadingIndicator(){
+        progressbar_load_indicator.visibility = View.VISIBLE
+    }
 
-    //TODO: 16 - Criar classe interna para rodar tarefa assíncrona
 
-    //TODO: 17 - Criar função para lançar a Task
+    fun hideLoadingIndicator(){
+        progressbar_load_indicator.visibility = View.GONE
+    }
+
+
+    inner class TaskAstros(): AsyncTask<Void, Int, List<AstrosPeople>>(){
+
+       private val repository = AstrosRepository()
+
+        override fun onPreExecute(){
+            super.onPreExecute()
+            showLoadingIndicator()
+        }
+
+
+        override fun doInBackground(vararg params: Void?): List<AstrosPeople> {
+            onProgressUpdate()
+            return repository.loadData()
+        }
+
+        override fun onPostExecute(result: List<AstrosPeople>){
+            super.onPostExecute(result)
+            hideLoadingIndicator()
+            showData(result)
+        }
+
+    }
+
+
+    fun launchAstrosTask(){
+        val task = TaskAstros()
+        task.execute()
+    }
 
 }
